cb3db22c3a51862e5bba81ffe6d414d3e61f00af
Marcel <me@m4rc3l.de>
2025-02-15 22:05:53 +0100

    misc: update deps

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,9 +1,8 @@
 /// <reference types="vitest" />
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import {resolve} from 'path';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
